a4999b599240da1417dc4a5cda21723c0641c944
beProsto <lavionperavion@gmail.com>
2022-11-21 12:10:36 +0100

    client

--- www/src/Login.svelte
+++ www/src/Login.svelte
@@ -1,14 +1,76 @@
 <script>
+    let classSelected = "";
+    const classes = [
+        "Nauczyciel",
+        "1mb",
+        "1wb",
+        "1at",
+        "1ct",
+        "1dt",
+        "1gt",
+        "1it",
+        "2mb",
+        "2wb",
+        "2act",
+        "2dgt",
+        "2it",
+        "2ist",
+        "3mb",
+        "3wb",
+        "3ast",
+        "3dgt",
+        "3it",
+        "4dP",
+        "4igP",
+        "4iP",
+        "4aG",
+        "4cgG",
+        "4dG",
+        "4iG"
+    ];
 
+    let name = "";
+    let surname = "";
+    let email = "";
+
+    function sendForm(e) {
+        e.preventDefault();
+        fetch("/submit_vote", {
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                first: 31,
+                second: 22,
+                third: 15,
+                name: name,
+                surname: surname,
+                class: classSelected,
+                email: email
+            })
+        })
+        .then(res => res.text())
+        .then(txt => {console.log("GOT:" + txt)});
+    }
 </script>
 
 <div class="login">
-    <form action="chujwie" class="login--form">
-        <input type="text" placeholder="Imie" class="login--name">
-        <input type="text" placeholder="Nazwisko" class="login--surname">
-        <input type="text" placeholder="Klasa" class="login--class">
-        <input type="mail" placeholder="Email" class="login--mail">
-        <input type="submit" class="login--submit">
+    <form on:submit={sendForm} class="login--form">
+        <input type="text" bind:value={name} placeholder="Imie" class="login--name" required>
+        <input type="text" bind:value={surname} placeholder="Nazwisko" class="login--surname" required>
+        <!-- <input type="option" placeholder="Klasa" class="login--class"> -->
+        <select bind:value={classSelected} required>
+            <option value="" disabled selected>Klasa</option>
+            {#each classes as c}
+                <option value={c}>
+                    {c}
+                </option>
+            {/each}
+        </select>
+        <input type="email" bind:value={email} placeholder="Email" class="login--mail" required>
+        <input type="submit" class="login--submit" required>
     </form>
 </div>
 
